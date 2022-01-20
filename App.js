091b38b1b56d7a1ce746047b1abde9547a87e15f
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-01-20 15:37:19 +0100

    Add Firebase config

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as FirebaseCore from 'expo-firebase-core';
+import { getApp, initializeApp } from 'firebase/app';
 
 export default function App() {
+
+  useEffect(() => {
+    if(FirebaseCore.DEFAULT_APP_OPTIONS)
+    initializeApp(FirebaseCore.DEFAULT_APP_OPTIONS);
+  }, [])
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
