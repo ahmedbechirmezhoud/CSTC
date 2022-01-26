848ca37f6d5ca6cbf71aa8bb42bf754b68a90e6e
M0ngi <saidanemongi@gmail.com>
2022-01-26 22:01:48 +0100

    Add 2 more error codes

--- const/errorCodes.js
+++ const/errorCodes.js
@@ -2,5 +2,6 @@ export const ErrorCodes = {
     NOT_LOGGED_IN: "user-not-logged-in",
     EMAIL_NOT_VERIFIED: "email-not-verified",
     FB_LOGIN_CANCEL: "facebook-login-canceled",
-    PHONE_ALREADY_INUSE: "phone-already-used"
+    PHONE_ALREADY_INUSE: "phone-already-used",
+    PHONE_DOESNT_EXIST: "phone-doesn't-exist"
 }
