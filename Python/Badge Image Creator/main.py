7b1d8db4c57171cd0a8580b20c62b2705895d534
M0ngi <saidanemongi@gmail.com>
2022-02-10 20:56:57 +0100

    Add badge generator

---
+++ Python/Badge Image Creator/main.py
@@ -0,0 +1,42 @@
+from operator import mod
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+import cv2
+
+
+# Make sure to download private key & update this
+cred = credentials.Certificate("cstc-2a071-firebase-adminsdk-p1uh4-48a1078766.json")
+app = firebase_admin.initialize_app(cred)
+fsClient = firestore.client(app)
+
+usersDoc = fsClient.collection("users")
+
+model = cv2.imread('model.jpg') # Todo: Replace test model
+
+users = auth.list_users(app=app)
+while users:
+    for user in users.users:
+        if user.display_name is None:
+            print('[!] User ID: {0} have no display name!'.format(user.uid))
+            continue
+
+        userDoc = usersDoc.document(user.uid)
+        userData = userDoc.get().to_dict()
+        
+        if userData['paidFee'] == False:
+            continue
+
+        print('[*] Created image for {0} (ID: {1})'.format(user.email, user.uid))
+        
+        img = model.copy()
+
+        # Todo: Update this
+        cv2.putText(img, user.display_name, (0, 100), cv2.FONT_ITALIC, 2, (0,0,0), 5)
+        cv2.putText(img, "Participant", (0, 200), cv2.FONT_ITALIC, 2, (0,0,0), 5)
+
+        cv2.imwrite('badges/{0}.jpg'.format(user.uid), img)
+    
+    users = users.get_next_page()
+
