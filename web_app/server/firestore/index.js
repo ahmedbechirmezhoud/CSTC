f5498622302273ebf244a012d7beb59cd473ddbf
M0ngi <saidanemongi@gmail.com>
2022-02-28 18:26:03 +0100

    Add getUsers route

--- web_app/server/firestore/index.js
+++ web_app/server/firestore/index.js
@@ -1,11 +1,28 @@
 const firebaseAdmin = require("firebase-admin");
+const async = require("async");
 
 const firestore = firebaseAdmin.app().firestore();
 const auth = firebaseAdmin.app().auth();
 
+const MAX_PER_PAGE = 2;
 
-module.exports.getUsers = () => {
-    let users = firestore.collection("users");
 
-    console.log(users)
+module.exports.getUsers = async (page) => {
+    let usersColl = firestore.collection("users");
+    let users = await auth.listUsers(MAX_PER_PAGE);
+    while(page != 0){
+        users = await auth.listUsers(MAX_PER_PAGE, users.pageToken);
+        page--;
+    }
+
+    let usersList = {};
+    let count = 0;
+    for(let i = 0; i < users.users.length; i++){
+        const jsonArr = users.users[i].toJSON();
+        const data = (await firestore.doc("users/"+jsonArr.uid).get()).data();
+
+        usersList[count++] = {uid: jsonArr.uid, email: jsonArr.email, displayName: jsonArr.displayName ?? "", checkedIn: data.checkedIn}
+    }
+
+    return usersList;
 };
--- web_app/server/index.js
+++ web_app/server/index.js
@@ -2,8 +2,14 @@ const { app } = require("./config");
 const express = require("express");
 const firestore = require('./firestore');
 
-app.get("/api/getUsers", (req, res) => {
-    res.json({ code: 200 });
-    firestore.getUsers();
+app.get("/api/getUsers", async (req, res) => {
+    let page = 0;
+    if(req.query.page) page = parseInt(req.query.page, 10);
+
+    try{
+        res.json({ code: 200, res: await firestore.getUsers(page) });
+    } catch(e){
+        res.json({ code: 500, error: e});
+    }
 });
 
