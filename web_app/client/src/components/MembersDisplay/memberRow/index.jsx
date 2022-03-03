2380a4af605f5c6af8f203c8317a15386bf1c100
M0ngi <saidanemongi@gmail.com>
2022-03-03 19:51:28 +0100

    Change ID Display, indicate that display name is empty

--- web_app/client/src/components/MembersDisplay/memberRow/index.jsx
+++ web_app/client/src/components/MembersDisplay/memberRow/index.jsx
@@ -4,7 +4,7 @@ export default function MemberRow(props){
     return (
         <tr className='memberRow'>
             <td className='selector'><input type='checkbox' className='clickable' onChange={props.onChange} checked={props.checked}/></td>
-            <td className='nameCol'><div>{props.name}</div><div>#12345</div></td>
+            <td className='nameCol'><div>{props.name === "" ? "[!] EMPTY NAME" : props.name}</div><div className='userID'>#{props.uid}</div></td>
             <td className='emailCol'>{props.email}</td>
             <td className='statCol'>
                 <div 
