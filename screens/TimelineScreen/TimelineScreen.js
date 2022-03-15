8f7eb6cfb545f77fcc89db8f71e54a72369cf33a
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-03-15 11:56:54 +0100

    Create Timeline Screen

--- screens/TimelineScreen/TimelineScreen.js
+++ screens/TimelineScreen/TimelineScreen.js
@@ -1,10 +1,37 @@
-import { View, Text } from "react-native";
+import { Dimensions, StyleSheet, Text, Image } from "react-native";
+import Background from "../../components/Background/Background";
+import { Card } from 'react-native-elements';
 
 
 export default function TimelineScreen(){
     return(
-        <View>
-            <Text>Timeline</Text>
-        </View>
+        <Background>
+            <Image
+                source={require('../../assets/logo-name-slogan.png')}
+            />
+            <Card title="Local Modules" containerStyle={styles.container} >
+                <Text style={styles.paragraph}>
+                    React Native Card View for Android and IOS using
+                    "react-native-elements"
+                </Text>
+            </Card>
+        </Background>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+        borderRadius: 20,
+        width: "100%",
+        height:Dimensions.get("screen").height
+    },
+    paragraph: {
+      margin: 24,
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      color: '#34495e'
+    },
+  });
+  
